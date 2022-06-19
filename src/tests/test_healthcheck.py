fec64a3dcb06e1f769a05fb664232b6b0abe3da1
Jakub Bacic <jakub.bacic@gmail.com>
2022-06-19 22:47:56 +0200

    fix mypy

--- src/tests/test_healthcheck.py
+++ src/tests/test_healthcheck.py
@@ -9,8 +9,10 @@ query Healthcheck {
 """
 
 
-async def test_healthcheck():
+async def test_healthcheck() -> None:
     resp = await schema.execute(QUERY)
 
     assert resp.errors is None
+    assert resp.data is not None
+
     assert resp.data["healthcheck"] == {"ok": True}
