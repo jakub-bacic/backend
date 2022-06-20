536d5c1a693a4de9d41b0c9c5c3b6a5698abbf9e
Jakub Bacic <jakub.bacic@gmail.com>
2022-06-20 11:12:20 +0200

    improve unit tests

--- src/tests/test_healthcheck.py
+++ src/tests/test_healthcheck.py
@@ -1,8 +1,8 @@
 from gql.schema import schema
 
 QUERY = """
-query Healthcheck {
-  healthcheck {
+query Healthcheck($works: Boolean!, $value: Int) {
+  healthcheck(works: $works, value: $value) {
     ok
   }
 }
@@ -10,9 +10,22 @@ query Healthcheck {
 
 
 async def test_healthcheck() -> None:
-    resp = await schema.execute(QUERY)
+    resp = await schema.execute(QUERY, variable_values={"works": True})
 
     assert resp.errors is None
     assert resp.data is not None
 
     assert resp.data["healthcheck"] == {"ok": True}
+
+
+async def test_healthcheck_should_fail() -> None:
+    resp = await schema.execute(QUERY, variable_values={"works": False})
+
+    assert resp.errors is not None
+
+
+async def test_healthcheck_with_value() -> None:
+    resp = await schema.execute(QUERY, variable_values={"works": True, "value": 666})
+
+    assert resp.errors is None
+    assert resp.data is not None
